2c69cf671b7d3ef3f3e4a4cd6abd5ce89602d781
s10myk4 <st4439@gmail.com>
2020-12-24 17:12:59 +0900

    add scalac options

--- project/BaseSettings.scala
+++ project/BaseSettings.scala
@@ -7,8 +7,14 @@ object BaseSettings {
     organization := "com.s10myk4",
     scalaVersion := "2.13.3",
     version := "1.0",
-    mainClass in(Compile, run) := Some("com.s10myk4.chatservice.Main"),
-    scalacOptions in Compile ++= Seq("-deprecation", "-feature", "-unchecked", "-Xlog-reflective-calls", "-Xlint"),
+    scalacOptions in Compile ++= Seq(
+      "-deprecation",
+      "-feature",
+      "-unchecked",
+      "-encoding",
+      "UTF-8",
+      "-Xlog-reflective-calls",
+      "-Xlint"),
     fork in run := false,
     parallelExecution in Test := false
   )
